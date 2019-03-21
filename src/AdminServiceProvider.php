7586444aef45741b37b0d5f24f4ad23df71b2595
v-sing <email1946367301@163.com>
2019-03-21 23:49:32 +0800

    嘎嘎嘎

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -35,12 +35,12 @@ class AdminServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->loadViewsFrom(__DIR__ . '/views', 'Admin'); // 视图目录指定
-        $this->publishes([
-            __DIR__ . '/views'            => base_path('resources/views/vendor/admin'),  // 发布视图目录到resources 下
-            __DIR__ . '/config/admin.php' => config_path('admin.php'), // 发布配置文件到 laravel 的config 下
-        ]);
-        $this->commands($this->commands);
+        // 单例绑定服务
+        $this->app->singleton('admin', function ($app) {
+            return new Admin($app['session'], $app['config']);
+        });
+
+//        $this->commands($this->commands);
     }
 
     /**
@@ -50,19 +50,16 @@ class AdminServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // 单例绑定服务
-        $this->app->singleton('admin', function ($app) {
-            return new Admin($app['session'], $app['config']);
-        });
+        $this->loadViewsFrom(future_path('/resources/views'), 'admin'); // 视图目录指定
+//        if($this->app->runningInConsole()){
+//            $this->publishes([
+//                future_path('/resources/views')  => $this->app->resourcePath('views/vendor/admin'),
+//            ], 'laravel-admin');
+//        }
+        $this->publishes([
+            future_path('/config/admin.php') => config_path('admin.php'), // 发布配置文件到 laravel 的config 下
+        ]);
 
     }
 
-    /**
-     * @return array
-     */
-    public function provides()
-    {
-        // 因为延迟加载 所以要定义 provides 函数 具体参考laravel 文档
-        return ['admin'];
-    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -6,3 +6,17 @@
  * Date: 2019/3/21 0021
  * Time: 14:19
  */
+if (!function_exists('future_path')) {
+
+    /**
+     * 返回库的根目录
+     * @param $file
+     * @return string
+     */
+    function future_path($file)
+    {
+
+        return dirname(__DIR__) . $file;
+    }
+
+}
